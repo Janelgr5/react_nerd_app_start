c8f636281738cc316d4f0146da9b2a88ad43ebc2
Janel Grant <janelgr5@gmail.com>
2019-10-10 17:40:41 -0400

    express boilerplate

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -5,4 +5,10 @@ router.use('/apiRouteOne', require('./apiRouteOne'));
 router.use('/apiRouteTwo', require('./apiRouteTwo'));
 router.use('/apiRouteThree', require('./apiRouteThree'));
 
+router.use(function (req, res, next) {
+  const err = new Error('Not found.');
+  err.status = 404;
+  next(err);
+});
+
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
+const port = process.env.PORT || 3000;
 // const router = express.Router();
 
 const app = express();
@@ -11,3 +13,17 @@ express.urlencoded({
 });
 
 app.use('/api', require('./apiRoutes'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+app.listen(port, function () {
+  console.log(`Your server, listening on port ${port}`);
+});
