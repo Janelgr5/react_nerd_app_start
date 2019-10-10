83f124e1d58ec36bf4c15af57c7531a4ed214ed0
Janel Grant <janelgr5@gmail.com>
2019-10-10 13:24:01 -0400

    middleware

--- server/index.js
+++ server/index.js
@@ -1,2 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('combined'));
+express.static('public');
+express.urlencoded({
+  extended: true
+});
